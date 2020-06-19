18fdeb442430f1c9f3d798b40eff65e8102af4d0
puhaojie <1293753897@qq.com>
2020-06-19 10:10:46 +0800

    添加基本包名

---
+++ app/src/main/java/com/lonbon/lonbon_app/activitys/A.java
@@ -0,0 +1,9 @@
+package com.lonbon.lonbon_app.activitys;
+
+/**
+ * 描述：
+ * Created by PHJ on 2020/6/19.
+ */
+
+public class A {
+}
---
+++ app/src/main/java/com/lonbon/lonbon_app/utils/A.java
@@ -0,0 +1,9 @@
+package com.lonbon.lonbon_app.utils;
+
+/**
+ * 描述：
+ * Created by PHJ on 2020/6/19.
+ */
+
+public class A {
+}
